ec79e6bdf9a00c8aea93a7ed860a7c2530dd8278
wayllertt <alidzhieva2005@mail.ru>
2025-11-11 01:08:55 +0300

    исправлен мэйн

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,1 +1,17 @@
 package api
+
+import (
+	"log"
+	"net/http"
+
+	"seller-metrics/app/app"
+)
+
+func main() {
+	handler := app.NewHTTPServer()
+
+	log.Println("Starting server on :8080")
+	if err := http.ListenAndServe(":8080", handler); err != nil {
+		log.Fatal(err)
+	}
+}
